7a535edadaceda93b664cca2eb56b14b9c92ec5e
Aghunik <aghunik.sahakyan@mail.ru>
2026-01-29 23:35:32 +0300

    init new files

---
+++ main.js
@@ -0,0 +1,12 @@
+let firstName = prompt('Введите ваше имя');
+alert('Привет, ' + firstName + '!');
+
+
+let old = +prompt ('Введите ваш год рождения');
+const thisYear = 2026;
+alert (thisYear - old);
+
+side = +prompt('Введите длину стороны квадрата');
+let S = side ** 2;
+alert('Площадь квадрата равна ' + S);
+
