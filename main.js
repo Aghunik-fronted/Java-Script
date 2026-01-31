c254c77882d0dbf3e4457ce893676cb5a9578cd6
Aghunik <aghunik.sahakyan@mail.ru>
2026-01-31 14:27:24 +0300

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let firstName = prompt('Введите ваше имя');
-alert('Привет, ' + firstName + '!');
+alert('Привет, ' + firstName + ' !');
 
 
 let old = +prompt ('Введите ваш год рождения');
@@ -7,6 +7,20 @@ const thisYear = 2026;
 alert (thisYear - old);
 
 side = +prompt('Введите длину стороны квадрата');
-let S = side ** 2;
-alert('Площадь квадрата равна ' + S);
+let P = side * 4;
+alert('Приметр квадрата равна ' + P + ' !');
 
+
+r = +prompt('Введите радиус окружности ');
+let S = 3.14 * r ** 2; 
+alert('Площадь окружности равна ' + S + ' !');
+
+distance = +prompt('Введите расстояние в км между двумя городами !');
+T = +prompt('Введите, за сколько часов хотите добраться !');
+let V = distance / T;
+alert ('Необходимо двигаться со скоростью ' + V + ' км/ч !');
+
+let usd = +prompt('Введите сумму в долларах (USD):');
+const toEur = 0.84;
+let eur = usd * toEur;
+alert(eur + ' евро !');
